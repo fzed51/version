71e1b6db74a1549e82b206429f309eb09c2f5348
Fabien Sanchez <fzed51+git@gmail.com>
2021-12-03 01:09:48 +0100

    feat / gestion des actions Cli
    features:
    - preRelease
    - set
    - usage

--- src/Version/Cli/SemVer.php
+++ src/Version/Cli/SemVer.php
@@ -25,18 +25,9 @@ class SemVer
      */
     public function get(): string
     {
-        if (!is_file($this->versionFile)) {
-            throw new CliMessageException(
-                "$this->versionFile n'est pas un fichier valide, veuillez utiliser le flag --init"
-            );
-        }
-        try {
-            $currentVersionJson = file_get_contents(realpath($this->versionFile));
-            $version = \Version\SemVer::fromJson($currentVersionJson);
+        $this->testFileVersion();
+            $version = $this->getVersionFromFileVersion();
             return "version courante : " . (string) $version;
-        } catch (Exception $e) {
-            throw new CliMessageException("Le fichier de version n'est pas un fichier de version valide");
-        }
     }
 
     /**
@@ -51,7 +42,7 @@ class SemVer
             );
         }
         $version = new \Version\SemVer(0, 0, 1);
-        file_put_contents($this->versionFile, json_encode($version, JSON_PRETTY_PRINT));
+        $this->setVersionToFileVersion($version);
         return "Le fichier de version {$this->versionFile} a été créé avec la version : $version";
     }
 
@@ -59,13 +50,9 @@ class SemVer
      * @param string $level
      * @return string
      */
-    public function increment(string $level)
+    public function increment(string $level): string
     {
-        if (!is_file($this->versionFile)) {
-            throw new CliMessageException(
-                "$this->versionFile n'est pas un fichier valide, veuillez utiliser le flag --init"
-            );
-        }
+        $this->testFileVersion();
         $currentVersionJson = file_get_contents(realpath($this->versionFile));
         $version = \Version\SemVer::fromJson($currentVersionJson);
         $out = "ancienne version : " . (string) $version . PHP_EOL;
@@ -85,9 +72,112 @@ class SemVer
                 );
         }
         $out.= "nouvelle version : " . (string) $version;
-        file_put_contents($this->versionFile, json_encode($version, JSON_PRETTY_PRINT));
+        $this->setVersionToFileVersion($version);
         return $out;
     }
 
+    /**
+     * Test la présence du fileVersion
+     */
+    protected function testFileVersion(): void
+    {
+        if (!is_file($this->versionFile)) {
+            throw new CliMessageException(
+                "$this->versionFile n'est pas un fichier valide, veuillez utiliser le flag --init"
+            );
+        }
+    }
 
+    /**
+     * @return \Version\SemVer
+     */
+    protected function getVersionFromFileVersion(): \Version\SemVer
+    {
+        try {
+            $currentVersionJson = file_get_contents(realpath($this->versionFile));
+            return \Version\SemVer::fromJson($currentVersionJson);
+        } catch (Exception $e) {
+            throw new CliMessageException("Le fichier de version n'est pas un fichier de version valide");
+        }
+    }
+
+    /**
+     * @param \Version\SemVer $version
+     */
+    protected function setVersionToFileVersion(\Version\SemVer $version): void
+    {
+        try {
+            file_put_contents($this->versionFile, json_encode($version, JSON_PRETTY_PRINT|JSON_THROW_ON_ERROR));
+        } catch (Exception $e) {
+            throw new CliMessageException("Le fichier de version n'est pas un fichier de version valide");
+        }
+    }
+
+    /**
+     * @param string $preRelease
+     * @return string
+     */
+    public function preRelease(string $preRelease, bool $force = false): string
+    {
+        $this->testFileVersion();
+        $version = $this->getVersionFromFileVersion();
+        $out = "ancienne version : " . (string) $version . PHP_EOL;
+        $oldVersion = clone $version;
+        $version->setPreRelease($preRelease);
+        if (!$force && $version->lt($oldVersion)) {
+            throw new CliMessageException(
+                "Vous allez assigner une version inférieure à la version actuelle. Pour effectuer quand même "
+                ."cette opération, utiliser le flag --force"
+            );
+        }
+        $out.= "nouvelle version : " . (string) $version;
+        $this->setVersionToFileVersion($version);
+        return $out;
+    }
+
+    public function set(string $strVersion, bool $force = false): string
+    {
+        $this->testFileVersion();
+        $oldVersion = $this->getVersionFromFileVersion();
+        $out = "ancienne version : " . (string) $oldVersion . PHP_EOL;
+        $version = \Version\SemVer::fromString($strVersion);
+        if (!$force && $version->lt($oldVersion)) {
+            throw new CliMessageException(
+                "Vous allez assigner une version inférieure à la version actuelle. Pour effectuer quand même "
+                ."cette opération, utiliser le flag --force"
+            );
+        }
+        $out.= "nouvelle version : " . (string) $version;
+        $this->setVersionToFileVersion($version);
+        return $out;
+    }
+
+    public function usage(): string
+    {
+        return <<<USAGE
+Usage:
+-------
+
+version-manager [--path chemin/version.json] [commande] [option]
+
+* --path -p [chemin] : indique la location du fichier de version
+                       ./version.json est pris comme valeur si le path n'est pas utilisé
+    
+Commandes :
+------------
+
+* sans commande        : lit le fichier de version
+* --init -i            : initialise le fichier de version (v0.0.1)
+* --increment -u [val] : incrément un element de la version 
+                         val = major | minor | patch
+* --preRelease -p [pr] : modifie l'élément preRelease de la version
+* --set -s [ver]       : modifie le numero de version 
+
+Options :
+----------
+
+* --force -f : force la commande (init, preRelease et set)
+* --help -h  : affiche ce message
+USAGE;
+    }
 }
--- test/Version/Cli/SemVerTest.php
+++ test/Version/Cli/SemVerTest.php
@@ -49,7 +49,7 @@ class SemVerTest extends TestCase
      *  Test de l'initialisation d'un fichier de version quand le fichier existe déjà
      *  -> test du message d'erreur
      */
-    public function testInitWithFileExist()
+    public function testInitWithFileExist(): void
     {
         $this->expectException(CliMessageException::class);
         $this->expectErrorMessage("./version.json existe déjà, pour forcer l'initialisation, utiliser le flag --force");
@@ -68,7 +68,7 @@ class SemVerTest extends TestCase
      *  Test de l'initialisation d'un fichier de version quand le fichier existe déjà
      *  -> test du flag "force"
      */
-    public function testForceInitWithFileExist()
+    public function testForceInitWithFileExist(): void
     {
 
         $cwd = getcwd();
@@ -101,6 +101,106 @@ class SemVerTest extends TestCase
         self::assertEquals('version courante : v0.1.0', $cli->get());
         self::assertEquals("ancienne version : v0.1.0\r\nnouvelle version : v1.0.0", $cli->increment('major'));
         self::assertEquals('version courante : v1.0.0', $cli->get());
+        chdir($cwd);
+    }
+
+    /**
+     * test la methode preRelease
+     */
+    public function testPreRelease(): void
+    {
+        $cwd = getcwd();
+        chdir(__DIR__."/../../fileVersion");
+        $cli = new SemVer();
+        $cli->init(true);
+        $cli->preRelease('alpha', true);
+        self::assertEquals('version courante : v0.0.1-alpha', $cli->get());
+        self::assertEquals(
+            "ancienne version : v0.0.1-alpha\r\nnouvelle version : v0.0.1-beta",
+            $cli->preRelease('beta')
+        );
+        self::assertEquals('version courante : v0.0.1-beta', $cli->get());
+        chdir($cwd);
+    }
+
+    /**
+     * test la methode preRelease avec une nouvelle version inferieure
+     */
+    public function testPreReleaseWithLightest(): void
+    {
+        $this->expectException(CliMessageException::class);
+        $cli = new SemVer(__DIR__."/../../fileVersion/version.json");
+        $cli->init(true);
+        $cli->preRelease('beta.2', true);
+        $cli->preRelease('beta.1');
+    }
+
+    /**
+     * test la methode preRelease avec une nouvelle version inferieure en forcant
+     */
+    public function testPreReleaseWithForcedLightest(): void
+    {
+        $cwd = getcwd();
+        chdir(__DIR__."/../../fileVersion");
+        $cli = new SemVer();
+        $cli->init(true);
+        $cli->preRelease('beta.2', true);
+        self::assertEquals('version courante : v0.0.1-beta.2', $cli->get());
+        self::assertEquals(
+            "ancienne version : v0.0.1-beta.2\r\nnouvelle version : v0.0.1-beta.1",
+            $cli->preRelease('beta.1', true)
+        );
+        self::assertEquals('version courante : v0.0.1-beta.1', $cli->get());
+        chdir($cwd);
+    }
+
+
+    /**
+     * test la methode set
+     */
+    public function testSet(): void
+    {
+        $cwd = getcwd();
+        chdir(__DIR__."/../../fileVersion");
+        $cli = new SemVer();
+        $cli->init(true);
+        self::assertEquals('version courante : v0.0.1', $cli->get());
+        self::assertEquals(
+            "ancienne version : v0.0.1\r\nnouvelle version : v1.0.0",
+            $cli->set('v1.0.0')
+        );
+        self::assertEquals('version courante : v1.0.0', $cli->get());
+        chdir($cwd);
+    }
+
+    /**
+     * test la methode set avec une version inferieure
+     */
+    public function testSetWithLightest(): void
+    {
+        $this->expectException(CliMessageException::class);
+        $cli = new SemVer(__DIR__."/../../fileVersion/version.json");
+        $cli->init(true);
+        $cli->set('v1.0.0');
+        $cli->set('v0.1.0');
+    }
+
+    /**
+     * test la methode set avec une version inferieure forcée
+     */
+    public function testSetWithLightestForced(): void
+    {
+        $cwd = getcwd();
+        chdir(__DIR__."/../../fileVersion");
+        $cli = new SemVer();
+        $cli->init(true);
+        $cli->set('v1.0.0');
+        self::assertEquals('version courante : v1.0.0', $cli->get());
+        self::assertEquals(
+            "ancienne version : v1.0.0\r\nnouvelle version : v0.0.1",
+            $cli->set('v0.0.1', true)
+        );
+        self::assertEquals('version courante : v0.0.1', $cli->get());
         chdir($cwd);
     }
 }
